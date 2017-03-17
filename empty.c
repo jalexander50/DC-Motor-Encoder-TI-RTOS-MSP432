f9f1dd0b9da1506d34b53393b4ab25d3ca02c036
jalexander50 <jalexander50@gmail.com>
2017-03-17 12:04:48 -0400

    First Commit

---
+++ empty.c
@@ -0,0 +1,149 @@
+/*
+ * Copyright (c) 2015, Texas Instruments Incorporated
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ *  ======== empty.c ========
+ */
+/* XDCtools Header files */
+#include <xdc/std.h>
+#include <xdc/runtime/System.h>
+
+/* BIOS Header files */
+#include <ti/sysbios/BIOS.h>
+#include <ti/sysbios/knl/Task.h>
+
+/* TI-RTOS Header files */
+#include <ti/drivers/GPIO.h>
+// #include <ti/drivers/I2C.h>
+// #include <ti/drivers/SDSPI.h>
+// #include <ti/drivers/SPI.h>
+// #include <ti/drivers/UART.h>
+// #include <ti/drivers/Watchdog.h>
+// #include <ti/drivers/WiFi.h>
+#include <ti/drivers/PWM.h>
+
+/* Board Header file */
+#include "Board.h"
+
+#define TASKSTACKSIZE   512
+
+Task_Struct task0Struct;
+Char task0Stack[TASKSTACKSIZE];
+
+int count = 0;
+
+/*
+ *  ======== heartBeatFxn ========
+ *  Toggle the Board_LED0. The Task_sleep is determined by arg0 which
+ *  is configured for the heartBeat Task instance.
+ */
+Void MotorFxn(UArg arg0, UArg arg1)
+{
+	PWM_Handle pwm1;
+	PWM_Params params;
+	uint16_t   pwmPeriod = 3000;
+	uint16_t   duty = 1000;
+
+	PWM_Params_init(&params);
+	params.dutyUnits = PWM_DUTY_US;
+	params.dutyValue = 0;
+	params.periodUnits = PWM_PERIOD_US;
+	params.periodValue = pwmPeriod;
+	pwm1 = PWM_open(Board_PWM0, &params);
+	    if (pwm1 == NULL) {
+	        System_abort("Board_PWM0 did not open");
+	    }
+	    PWM_start(pwm1);
+	while (1) {
+	      PWM_setDuty(pwm1, duty);
+	      Task_sleep(50);
+	}
+
+}
+
+Void encoderAFxn(){
+	count++;
+}
+
+Void encoderBFxn(){
+	count++;
+}
+
+/*
+ *  ======== main ========
+ */
+int main(void)
+{
+    Task_Params taskParams;
+
+    /* Call board init functions */
+    Board_initGeneral();
+    Board_initGPIO();
+    Board_initPWM();
+    // Board_initI2C();
+    // Board_initSDSPI();
+    // Board_initSPI();
+    Board_initUART();
+    // Board_initWatchdog();
+    // Board_initWiFi();
+
+    /* Construct heartBeat Task  thread */
+    Task_Params_init(&taskParams);
+    taskParams.arg0 = 1000;
+    taskParams.stackSize = TASKSTACKSIZE;
+    taskParams.stack = &task0Stack;
+    Task_construct(&task0Struct, (Task_FuncPtr)MotorFxn, &taskParams, NULL);
+
+
+
+    /* Turn on user LED */
+    GPIO_write(Board_LED0, Board_LED_ON);
+
+    System_printf("Starting the example\nSystem provider is set to SysMin. "
+                  "Halt the target to view any SysMin contents in ROV.\n");
+    /* SysMin will only print to the console when you call flush or exit */
+    System_flush();
+
+
+
+    GPIO_setCallback(INA, encoderAFxn);
+    GPIO_setCallback(INB, encoderBFxn);
+    GPIO_enableInt(INA);
+    GPIO_enableInt(INB);
+
+//    GPIO_write(PIN_4_1,1);
+//    GPIO_write(PIN_4_6,0);
+    /* Start BIOS */
+    BIOS_start();
+
+    return (0);
+}
